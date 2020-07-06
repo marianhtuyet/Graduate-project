f86b795933ace32a42004dd97605b6a4ece72f44
marianhtuyet <marianhtuyet2212@gmail.co>
2020-07-06 21:45:10 +0700

    Add view in xml for all record

---
+++ custom_addons/Caculate_score/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Calculate score',
+    'summary': 'Add report to caculate score for student',
+    'description': """
+Calculate score
+============================
+
+This module will calculate the score of student and arrange score for student
+        """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/class.xml',
+        'views/school_year.xml',
+        'views/student.xml',
+        'views/subject.xml',
+        'menu/menu.xml',
+    ],
+    'installable': True,
+}
--- custom_addons/Caculate_score/models/__init__.py
+++ custom_addons/Caculate_score/models/__init__.py
@@ -1,1 +1,5 @@
-from . import caculate_score.py
+from . import caculate_score
+from . import school_year
+from . import student
+from . import study_class
+from . import subject
